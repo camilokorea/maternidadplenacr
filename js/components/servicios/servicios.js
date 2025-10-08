e4bdf1df05c836b094b664306bfeb4331454d9ab
Camilo Corea Piedra <naturalcodedev@gmail.com>
2025-10-07 18:32:28 -0600

    Maternidad Plena CR Landing Page

--- js/components/servicios/servicios.js
+++ js/components/servicios/servicios.js
@@ -1,2 +1,19 @@
-function ServiciosController() {
+function ServiciosController($scope, $mdDialog) {
+    $scope.showDialog = function (dialog_target) {
+        $mdDialog.show({
+            controller: function ($scope, $mdDialog) {
+                $scope.hide = function () {
+                    $mdDialog.hide();
+                };
+            },
+            templateUrl: 'js/components/servicios/templates/' + dialog_target + '.html',
+            parent: angular.element(document.body),
+            clickOutsideToClose: true,
+            fullscreen: true
+        }).then(function (answer) {
+            console.log('answer: ' + answer);
+        }, function () {
+            console.log('cancelled');
+        });
+    };
 }
